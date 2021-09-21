6fef0b360cfff00ed971f7f98c2ec1f1b8fa81df
Dmytro Anokhin <5136301+dmytro-anokhin@users.noreply.github.com>
2021-09-21 22:56:11 +0200

    Autocomplete project

---
+++ Autocomplete/AutocompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutocompleteApp.swift
+//  Autocomplete
+//
+//  Created by Dmytro Anokhin on 21/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AutocompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Autocomplete/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Autocomplete
+//
+//  Created by Dmytro Anokhin on 21/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
